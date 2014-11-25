ae8b6d93483ed9fc4ff6cf6c8d19f04b5e453e73
Ethan Katzenberg <ethankatzenberg@Ethans-MacBook-Pro.local>
2014-11-25 22:08:32 +0000

    Partial buffered pipeline

--- asm/simple.asm
+++ asm/simple.asm
@@ -1,3 +1,23 @@
 start
-	mov $r0, 10
+	mov $r0, #10
+	nop
+	nop
+	nop
+	nop
+	nop
+	add $r1, $r0, #20
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	mul $r2, $r1, $r1
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
 	hlt
