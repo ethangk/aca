00af962e81e3681f072e186550389df6d2c3b089
Ethan Katzenberg <ethankatzenberg@Ethans-MacBook-Pro.local>
2014-11-30 20:01:23 +0000

    Baisc pipeline with buffer working

--- asm/simple.asm
+++ asm/simple.asm
@@ -1,23 +1,14 @@
 start
 	mov $r0, #10
-	nop
-	nop
-	nop
-	nop
-	nop
-	add $r1, $r0, #20
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	mul $r2, $r1, $r1
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
+	mov $r1, #20
+	mov $r2, #123
+	mov $r3, #10
+	mov $r4, #20
+	mov $r5, #123
+	mov $r6, #10
+	mov $r7, #20
+	mov $r8, #123
+	mov $r9, #10
+	mov $r10, #20
+	mov $r11, #123
 	hlt
