97c6e8406c50cf968f07dbdaa0adeaf66ca53ad8
Ethan Katzenberg <ethankatzenberg@Ethans-MacBook-Pro.local>
2014-12-16 14:08:50 +0000

    Working with all test files so far, issue was not clearning units

---
+++ asm/simple2.asm
@@ -0,0 +1,23 @@
+start
+	mov $r0, #100
+	mov $r1, #100
+	mov $r2, #200
+	mov $r3, #300
+	mov $r4, #400
+	mov $r5, #500
+	mov $r6, #600
+	mov $r7, #700
+	mov $r8, #800
+	mov $r9, #900
+	mov $r10, #1100
+	mov $r11, #1100
+	mov $r12, #1200
+	mov $r13, #1300
+	mov $r14, #1400
+	mov $r15, #1500
+	mov $r16, #1600
+	mov $r17, #1700
+	mov $r18, #1800
+	mov $r19, #1900
+end
+	hlt
---
+++ asm/sortSimple.asm
@@ -0,0 +1,30 @@
+start
+	mov $r0, #0
+	ldr $r1, numItems
+	mul $r1, $r1, #4
+	mov $r2, #0
+	add $r10, $r10, #4
+loopStart
+	mov $r0, #0
+	beq end, $r1, $r2
+	sub $r1, $r1, #4
+innerLoop
+	beq loopStart, $r0, $r1
+	add $r3, $r0, $r10
+	ldrn $r4, [$r0,array]
+	ldrn $r5, [$r3,array]
+	brc swap
+branchBack
+	add $r0, $r0, #4
+	jmp innerLoop
+swap
+	blt branchBack, $r4, $r5
+	strn $r4, [$r3,array]
+	strn $r5, [$r0,array]
+	jmp branchBack
+end
+	hlt
+numItems
+	.word 9
+array
+	.word 10,9,8,7,6,5,4,3,2
