391d18537759c66590d2f74074bd27e9c356dcec
Ethan Katzenberg <ethankatzenberg@Ethans-MacBook-Pro.local>
2014-12-16 12:56:59 +0000

    Kinda working, problem with some memory stuff in sort2

--- asm/simple.asm
+++ asm/simple.asm
@@ -3,13 +3,14 @@ start
 	mov $r1, #100
 	mov $r2, #200
 	mov $r3, #300
-	beq end, $r2, $r0
+	beq later, $r2, $r0
 	mov $r2, $r3
 	mov $r4, #400
 	mov $r5, #500
-end
+later
 	mov $r6, #600
 	mov $r7, #700
 	mov $r8, #800
 	mov $r9, #900
+end
 	hlt
